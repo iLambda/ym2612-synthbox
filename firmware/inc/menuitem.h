34acc2b8b46bf434e7e31f69fdd44496b1907241
Matthieu Freeman <me@ilambda.io>
2016-11-12 04:38:31 +0100

    Added more display and compute methods

--- firmware/inc/menuitem.h
+++ firmware/inc/menuitem.h
@@ -18,14 +18,23 @@ typedef struct ym2612menuitem{
 void menuitem_makechild(ym2612menuitem_t* parent, ym2612menuitem_t* child);
 void menuitem_makesibling(ym2612menuitem_t* base, ym2612menuitem_t* sibling);
 
+unsigned char menuitem_range_2(unsigned char value);
+unsigned char menuitem_range_4(unsigned char value);
 unsigned char menuitem_range_8(unsigned char value);
 unsigned char menuitem_range_16(unsigned char value);
 unsigned char menuitem_range_32(unsigned char value);
 unsigned char menuitem_range_64(unsigned char value);
 unsigned char menuitem_range_127(unsigned char value);
 unsigned char menuitem_range_detune(unsigned char value);
+unsigned char menuitem_range_lfo(unsigned char value);
 
+void menuitem_display_algo(unsigned char value, char* out);
+void menuitem_display_ams(unsigned char value, char* out);
+void menuitem_display_fms(unsigned char value, char* out);
+void menuitem_display_bool(unsigned char value, char* out);
 void menuitem_display_detune(unsigned char value, char* out);
+void menuitem_display_lfo(unsigned char value, char* out);
 void menuitem_display_mul(unsigned char value, char* out);
+void menuitem_display_rs(unsigned char value, char* out);
 
 #endif
--- firmware/src/menuitem.c
+++ firmware/src/menuitem.c
@@ -20,7 +20,15 @@ void menuitem_makesibling(ym2612menuitem_t* base, ym2612menuitem_t* sibling) {
 
 
 unsigned char menuitem_range_detune(unsigned char value) {
-  return ((char)value) >= 3 ? 3 : (((char)value) <= -3 ? ((unsigned char)-3) : value);
+  if (value >= 128) {
+    return value <= 0xFD ? 0xFD : value;
+  } else {
+    return value >= 0x03 ? 0x03 : value;
+  }
+}
+
+unsigned char menuitem_range_2(unsigned char value) {
+  return value % 2;
 }
 
 unsigned char menuitem_range_8(unsigned char value) {
@@ -43,6 +51,10 @@ unsigned char menuitem_range_128(unsigned char value) {
   return value % 128;
 }
 
+unsigned char menuitem_range_lfo(unsigned char value) {
+  return value % 9;
+}
+
 void menuitem_display_detune(unsigned char value, char* out) {
   if(value != 0) {
     strcpy(out, "x1  \x03");
@@ -59,5 +71,64 @@ void menuitem_display_mul(unsigned char value, char* out) {
     strcpy(out, "x\01");
   } else {
     out[value < 10 ? 1 : 0] = 'x';
+  }
+}
+
+void menuitem_display_bool(unsigned char value, char* out) {
+  strcpy(out, value ? "On" : "Off");
+}
+
+void menuitem_display_ams(unsigned char value, char* out) {
+  switch (value) {
+    case 0: strcpy(out, "0"); break;
+    case 1: strcpy(out, "1.4/dB"); break;
+    case 2: strcpy(out, "5.9/dB"); break;
+    case 3: strcpy(out, "11.8/dB"); break;
+  }
+}
+
+void menuitem_display_fms(unsigned char value, char* out) {
+  switch (value) {
+    case 0: strcpy(out, "\x040\x25"); break;
+    case 1: strcpy(out, "\x043.4\x25"); break;
+    case 2: strcpy(out, "\x046.7\x25"); break;
+    case 3: strcpy(out, "\x0410\x25"); break;
+    case 4: strcpy(out, "\x0414\x25"); break;
+    case 5: strcpy(out, "\x0420\x25"); break;
+    case 6: strcpy(out, "\x0440\x25"); break;
+    case 7: strcpy(out, "\x0480\x25"); break;
+  }
+}
+
+
+void menuitem_display_rs(unsigned char value, char* out) {
+  strcpy(out, "1/ ");
+  out[2] = 0x30 + (8 >> value);
+}
+
+void menuitem_display_lfo(unsigned char value, char* out) {
+  switch (value) {
+    case 0: strcpy(out, "Off"); break;
+    case 1: strcpy(out, "3.98Hz"); break;
+    case 2: strcpy(out, "5.56Hz"); break;
+    case 3: strcpy(out, "6.02Hz"); break;
+    case 4: strcpy(out, "6.37Hz"); break;
+    case 5: strcpy(out, "6.88Hz"); break;
+    case 6: strcpy(out, "9.63Hz"); break;
+    case 7: strcpy(out, "48.10Hz"); break;
+    case 8: strcpy(out, "72.20Hz"); break;
+  }
+}
+
+void menuitem_display_algo(unsigned char value, char* out) {
+  switch (value) {
+    case 0: strcpy(out, "1>2>3>4"); break;
+    case 1: strcpy(out, "12>3>4>"); break;
+    case 2: strcpy(out, "1(2>3)>4"); break;
+    case 3: strcpy(out, "(1>2)3>4"); break;
+    case 4: strcpy(out, "(1>2)(3>4)"); break;
+    case 5: strcpy(out, "1>234>"); break;
+    case 6: strcpy(out, "(1>2)34>"); break;
+    case 7: strcpy(out, "1234>"); break;
   }
 }
